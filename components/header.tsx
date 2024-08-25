f223917b568ed5c5a23ac99ca28c84450d0cde50
zykcode <zyskson@gmail.com>
2024-08-25 23:39:50 +0100

    feat: feat: page

--- components/header.tsx
+++ components/header.tsx
@@ -20,27 +20,28 @@ const Header = () => {
       animate={isOpen ? 'open' : 'closed'}
       className="flex justify-between items-center h-18 md:h-20 sticky top-0 z-50 bg-white/80 dark:bg-gray-900/80 py-2 backdrop-blur-sm"
     >
-      <div className="md:hidden flex bg-yellow-400 self-center items-center h-8 w-8 rounded-full">
-        <Link href="/profile">
-          {isSignedIn ? (
-            <UserButton
-              appearance={{
-                elements: {
-                  avatarImage: 'h-full w-full',
-                },
-              }}
+      <Link
+        href="/profile"
+        className="md:hidden flex bg-yellow-400 self-center items-center h-8 w-8 rounded-full"
+      >
+        {isSignedIn ? (
+          <UserButton
+            appearance={{
+              elements: {
+                avatarImage: 'h-full w-full',
+              },
+            }}
+          />
+        ) : (
+          <Avatar>
+            <AvatarImage
+              src="/images/generic-avatar.png"
+              alt="Generic Avatar"
             />
-          ) : (
-            <Avatar>
-              <AvatarImage
-                src="/images/generic-avatar.png"
-                alt="Generic Avatar"
-              />
-              <AvatarFallback>GA</AvatarFallback>
-            </Avatar>
-          )}
-        </Link>
-      </div>
+            <AvatarFallback>GA</AvatarFallback>
+          </Avatar>
+        )}
+      </Link>
 
       <Breadcrumbs />
 
